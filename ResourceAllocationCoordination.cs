b32150c2bf4eef16dedd5acfa662043e87947edb
Paweł Korpal <pawel.korpal@adm.uw.edu.pl>
2019-06-06 18:30:30 +0200

    added ResoureAllocationCoordination.cs to project

---
+++ ResourceAllocationCoordination.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCC
+{
+    class ResourceAllocationCoordination
+    {
+        public ResourceAllocationCoordination()
+        {
+
+        }
+
+        public bool resourceAllocation()
+        {
+            return false;
+        }
+    }
+}
